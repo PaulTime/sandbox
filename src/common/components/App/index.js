d9898d630a883804857ef50c055c3841f612820b
Pavlo Timofieiev <pavliashvili@ukr.net>
2018-06-16 23:45:57 +0300

    add cn decorators, decorators plugin, app layout, and routes

--- src/common/components/App/index.js
+++ src/common/components/App/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import bemDecorator from 'cn-decorator';
 import { Switch, Route } from 'react-router-dom';
 
+import AppLayout from 'common/layouts/AppLayout'
+
+@bemDecorator('app-root')
 export default class App extends React.Component {
-    render () {
+    render (bem) {
         return (
-            <Switch>
-                <Route exact path="/" component={() => <h1>header 1</h1>}/>
-                <Route exact path="/landing-credits" component={() => <h1>landing-credits</h1>}/>
-            </Switch>
+            <section className={bem()}>
+                <AppLayout>
+                    <Switch>
+                        <Route exact path="/" component={() => <h1>header 1</h1>}/>
+                        <Route exact path="/landing-credits" component={() => <h1>landing-credits</h1>}/>
+                    </Switch>
+                </AppLayout>
+            </section>
         )
     }
 };
